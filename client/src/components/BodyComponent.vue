7e59e39e93b24737d89ffa0274069457cc27c7be
Yash Pahade <yash.aries.6@gmail.com>
2019-10-22 22:05:18 -0700

    Delete working on Vue front end

--- client/src/components/BodyComponent.vue
+++ client/src/components/BodyComponent.vue
@@ -4,25 +4,27 @@
     <hr>
     <p class="error" v-if="error"> {{error}} </p>
     <div class="post-container">
-      <div class ="post"
-        v-for="(poem, index) in poems"
-        v-bind:item="poem"
-        v-bind:index="index"
-        v-bind:key="poem._id"
-      >
-        {{ poem.createdAt.getDate() }}
-        <p class="text">{{ poem.text }}</p>
-      </div>
+      <template v-for="(poem, index) in poems">
+        <PoemComponent
+          v-bind:poemObj="poem"
+          v-bind:index="index"
+          v-bind:key="poem._id"
+          v-on:deletePoem="updateBody"
+        />
+      </template>
     </div>
   </div>
-  
 </template>
 
 <script>
 import PoemService from '../PoemService'
+import PoemComponent from './PoemComponent.vue'
 
 export default {
   name: 'BodyComponent',
+  components: {
+    PoemComponent
+  },
   data() {
     return {
       poems: [],
@@ -36,44 +38,25 @@ export default {
     } catch(err) {
       this.error = err.message;
     }
-  }
+  },
+  methods: {
+    async updateBody () {
+      try {
+        this.poems = await PoemService.getPoems();
+      } catch(err) {
+        this.error = err.message;
+      }
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 div.container {
   max-width: 800px; 
   margin: 0 auto; 
-}
-
-p.error { 
-  border: 1px solid #ff5b5f; 
-  background-color: #ffc5c1; 
-  padding: 10px; 
-  margin-bottom: 15px; 
-}
-
-div.post { 
-  position: relative; 
-  border: 1px solid #5bd658; 
-  background-color: #bcffb8; 
-  padding: 10px 10px 30px 10px; 
-  margin-bottom: 15px; 
-}
-
-div.created-at {
-  position: absolute; 
-  top: 0; 
-  left: 0; 
-  padding: 5px 15px 5px 15px; 
-  background-color: darkgreen; 
-}
-
-p.text { 
-  font-size: 22px; 
-  font-weight: 700; 
-  margin-bottom: 0; 
 }
 
 </style>
--- client/src/components/PoemComponent.vue
+++ client/src/components/PoemComponent.vue
@@ -1,12 +1,52 @@
 <template>
-
+    <div class="post">
+        {{ poemObj.createdAt.getMonth() + '/' + poemObj.createdAt.getDate()  + '/' + poemObj.createdAt.getFullYear()}}
+        <p class="text">{{ poemObj.text }}</p>
+        <button v-on:click="deletePoem"> Delete </button>
+    </div>
 </template>
 
 <script>
+import PoemService from '../PoemService'
 
+export default {
+    name: 'PoemComponent',
+    props: {
+        poemObj: Object,
+        key: String
+    },
+    methods: {
+      async deletePoem(){
+        await PoemService.deletePoem(this.poemObj._id);
+        this.$emit('deletePoem');
+      }
+    },
+}
 
 </script>
 
 <style scoped>
+
+div.post { 
+  position: relative; 
+  border: 1px solid #5bd658; 
+  background-color: #bcffb8; 
+  padding: 10px 10px 30px 10px; 
+  margin-bottom: 15px; 
+}
+
+div.created-at {
+  position: absolute; 
+  top: 0; 
+  left: 0; 
+  padding: 5px 15px 5px 15px; 
+  background-color: darkgreen; 
+}
+
+p.text { 
+  font-size: 22px; 
+  font-weight: 700; 
+  margin-bottom: 0; 
+}
 
 </style>
